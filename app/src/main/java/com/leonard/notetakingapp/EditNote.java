cb6a95515864946cd037b5ada3848a5cca576d7f
Leonard <muthonileo@gmail.com>
2020-03-13 07:57:45 +0300

    pre-final

--- app/src/main/java/com/leonard/notetakingapp/EditNote.java
+++ app/src/main/java/com/leonard/notetakingapp/EditNote.java
@@ -1,6 +1,8 @@
 package com.leonard.notetakingapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,10 +11,17 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
-public class EditNote extends AppCompatActivity {
+import java.util.HashSet;
+
+import static com.leonard.notetakingapp.MainActivity.notes;
+import static com.leonard.notetakingapp.MainActivity.set;
+
+public class EditNote extends AppCompatActivity implements TextWatcher {
 
     int noteId;
 
@@ -23,25 +32,66 @@ public class EditNote extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+//Editing an editText
         EditText editText=findViewById(R.id.editText);
         Intent i=getIntent();
 
         noteId=i.getIntExtra("noteId",-1);
 
         if (noteId !=-1){
-            String fillerText=MainActivity.notes.get(noteId);
+            String fillerText= notes.get(noteId);
             editText.setText(fillerText);
         }
+
+//        Automatically saving the note
+        editText.addTextChangedListener(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+
+                startActivity(intent);
+
             }
         });
+
+//        BACK ARROW
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+        notes.set(noteId,String.valueOf(charSequence));
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        SharedPreferences sharedPreferences=this.getSharedPreferences("com.leonard.notetakingapp", Context.MODE_PRIVATE);
+
+        if (set==null){
+            set=new HashSet<String>();
+        }else
+        {
+            set.clear();
+        }
+
+        set.clear();
+        set.addAll(notes);
+        sharedPreferences.edit().remove("notes").apply();
+        sharedPreferences.edit().putStringSet("notes",set).apply();
+
+
+//        to save data permanently create shared preferences in the main activity
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
 }
--- app/src/main/java/com/leonard/notetakingapp/MainActivity.java
+++ app/src/main/java/com/leonard/notetakingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.leonard.notetakingapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -20,11 +22,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
 
     static ArrayList<String>notes=new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+    static Set<String>set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +41,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         ListView listView=findViewById(R.id.listView);
+
+
+//        Saving data permamently
+
+        SharedPreferences sharedPreferences=this.getSharedPreferences("com.leonard.notetakingapp",Context.MODE_PRIVATE);
+        set=sharedPreferences.getStringSet("notes",null);
+        notes.clear();
+
+        if(set !=null){
+            notes.addAll(set);
+        }else
+        {
+            notes.add("Example note");
+            set=new HashSet<String>();
+            set.addAll(notes);
+            sharedPreferences.edit().remove("notes").apply();
+            sharedPreferences.edit().putStringSet("notes",set).apply();
+        }
+
+
+
+
         notes.add("Example note");
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+         arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -48,13 +76,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Delete")
+                        .setMessage("Do you want to delete this item?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(position);
+                                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences("com.leonard.notetakingapp", Context.MODE_PRIVATE);
+
+                                if (set==null){
+                                    set=new HashSet<String>();
+                                }else
+                                {
+                                    set.clear();
+                                }
+
+                                set.addAll(notes);
+
+//                Testing ...not necessary.
+
+                                sharedPreferences.edit().remove("notes").apply();
+                                sharedPreferences.edit().putStringSet("notes",set).apply();
+                                arrayAdapter.notifyDataSetChanged();
+
+
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+            }
+        });
+
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                notes.add("");
+                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences("com.leonard.notetakingapp", Context.MODE_PRIVATE);
+
+                if (set==null){
+                    set=new HashSet<String>();
+                }else
+                {
+                    set.clear();
+                }
+
+                set.addAll(notes);
+
+//                Testing ...not necessary.
+
+                sharedPreferences.edit().remove("notes").apply();
+                sharedPreferences.edit().putStringSet("notes",set).apply();
+                arrayAdapter.notifyDataSetChanged();
+
+
+
+
                 Intent intent=new Intent(getApplicationContext(),EditNote.class);
+
+                intent.putExtra("noteId",notes.size()-1);
                 startActivity(intent);
             }
         });
